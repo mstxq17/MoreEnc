4c391b2434abfbb7fe0727421c6e741afd6d4f61
mstxq17 <154380808@qq.com>
2023-02-25 01:07:07 +0800

    tests: add test case

---
+++ internal/core/encode_test.go
@@ -0,0 +1,58 @@
+package core
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+/*
+* Reference: https://geektutu.com/post/quick-go-test.html
+ */
+func setup() {
+	fmt.Println("Before all tests")
+}
+
+func teardown() {
+	fmt.Println("After all tests")
+
+}
+
+func TestUrlEncode(t *testing.T) {
+	cases := []struct {
+		Name            string
+		Input, Expected string
+	}{
+		{"1", "=", "%3D"},
+	}
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			if ans := UrlEncode(c.Input); ans != c.Expected {
+				t.Fatalf("%s expected %s, but %s got", c.Input, c.Expected, ans)
+			}
+		})
+	}
+}
+
+func TestBase64Encode(t *testing.T) {
+	cases := []struct {
+		Name            string
+		Input, Expected string
+	}{
+		{"1", "%", "JQ=="},
+	}
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			if ans := Base64Encode(c.Input); ans != c.Expected {
+				t.Fatalf("%s expected %s, but %s got", c.Input, c.Expected, ans)
+			}
+		})
+	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
